bb69f8e5e27e695ecb69aecc7e53611b3dee6e48
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-09-25 00:58:04 +0300

    Add files via upload

---
+++ Lab1/Программа/Построение графиков/error/f1Cheb/f1Ch.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+
+# Чтение данных из файла
+x5 = []
+y5 = []
+x7 = []
+y7 = []
+x10 = []
+y10 = []
+with open('cheb1_5.txt', 'r') as file:
+    for line in file:
+        x, y = map(float, line.split(':'))
+        x5.append(x)
+        y5.append(y)
+
+with open('cheb1_7.txt', 'r') as file:
+    for line in file:
+        x, y = map(float, line.split(':'))
+        x7.append(x)
+        y7.append(y)
+
+with open('cheb1_10.txt', 'r') as file:
+    for line in file:
+        x, y = map(float, line.split(':'))
+        x10.append(x)
+        y10.append(y)
+        
+# Построение графика с еще более тонкой линией и выбором цвета
+plt.plot(x5, y5, linewidth=1, color='green', label='5 узлов')
+plt.plot(x7, y7, linewidth=1, color='red', label='7 узлов')
+plt.plot(x10, y10, linewidth=1, color='blue', label='10 узлов')
+
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('поточечная ошибка на функции(1) на Чебышевской сетки')
+
+plt.grid(True)
+plt.legend(loc='upper right')  # Добавление легенды
+plt.show()
