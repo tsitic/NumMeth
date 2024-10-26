0485de56a4b18c13c81093edbbced90cb4321ec5
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-10-27 01:21:29 +0300

    Update main.cpp

--- Lab3/Программка/main.cpp
+++ Lab3/Программка/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <locale.h>
+#include <vector>
 #include <math.h>
 
 double f(double x, double y) {
@@ -10,80 +11,107 @@ double rf(double x) {
     return (1 - log(fabs(x))) / x;
 }
 
-//фиксированный шаг
-void methfs(double x0, double y0, double h, double xn) {
-    
+//С„РёРєСЃРёСЂРѕРІР°РЅРЅС‹Р№ С€Р°Рі
+std::vector<std::pair<double, double>> methfs(double x0, double y0, double h, double xn) {
     double x = x0;
     double y = y0;
+    std::vector<std::pair<double, double>> sol; 
 
-    std::cout << "h =" << h << "\n";
-    std::cout << x << " : " << y << "\n";
+    sol.push_back({ x, y });
+
     while (x < xn) {
         double yv = y + h * f(x, y); 
         y = y + h * (f(x, y) + f(x + h, yv)) / 2; 
         x += h;
-        std::cout << x << " : " << y << "\n";
+
+        sol.push_back({ x, y });
     }
-    std::cout << "\n";
+
+    return sol;
 }
 
-//точность
-void methnfs(double x0, double y0, double h, double xn, double e) {
+
+
+//С‚РѕС‡РЅРѕСЃС‚СЊ
+std::vector<std::pair<double, double>> methnfs(double x0, double y0, double h, double xn, double e) {
     double x = x0;
     double y = y0;
+    std::vector<std::pair<double, double>> sol;
 
-    double error = 0;
+    sol.push_back({ x, y });
+
     while (x < xn) {
+        double yv = y + h * f(x, y);
+        double y1 = y + h * (f(x, y) + f(x + h, yv)) / 2;
+
+        double y22;
         while (true) {
-            //шаг h
-            double yv = y + h * f(x, y);
-            double y1 = y + h * (f(x, y) + f(x + h, yv)) / 2;
-
-            //шаг h/2
             double h05 = h / 2;
-            yv = y + h05 * f(x, y);
-            double y21 = y + h05 * (f(x, y) + f(x + h05, yv)) / 2;
+            //       h/2
+            double yv_half = y + h05 * f(x, y);
+            double y21 = y + h05 * (f(x, y) + f(x + h05, yv_half)) / 2;
 
-            yv = y21 + h05 * f(x + h05, y21);
-            double y22 = y21 + h05 * (f(x + h05, y21) + f(x + h, yv)) / 2;
+            yv_half = y21 + h05 * f(x + h05, y21);
+            y22 = y21 + h05 * (f(x + h05, y21) + f(x + h, yv_half)) / 2;
 
             double runge = fabs(y1 - y22) / 3;
 
             if (runge < e) {
-                x += h;  
+                x += h;
                 y = y1;
-                error = fabs(rf(x) - y) > error ? fabs(rf(x) - y) : error;
 
-                std::cout << x << " : " << h << "\n";
+                sol.push_back({ x, y });
 
                 if (runge < e / 10) {
                     h *= 2;
                 }
+
+                y1 = y22;
                 break;
             }
             else {
-                h /= 2;
+                if (h > 1e-10) {
+                    h /= 2;
+                }
+                else {
+                    break;
+                }
             }
         }
     }
-    std::cout << error<<" : " <<e<< "\n";
+
+    return sol;
 }
 
 
+void analyzeSol(const std::vector<std::pair<double, double>>& sol, double e) {
+    double max_error = 0;
 
+    std::cout << "e: " << e << "\n";
+
+    for (size_t i = 1; i < sol.size(); ++i) {
+        double xp = sol[i - 1].first;
+        double yp = sol[i - 1].second;
+        double x = sol[i].first;
+        double y = sol[i].second;
+
+        double h_dot = x - xp;
+
+        double error = std::fabs(rf(x) - y);
+        max_error = std::max(max_error, error);
+
+        std::cout << "x = " << x << ", y = " << y
+            << ", h = " << h_dot
+            << ", error: " << error << "\n";
+    }
+
+    std::cout << "РњР°РєСЃРёРјР°Р»СЊРЅР°СЏ РѕС€РёР±РєР°: " << max_error << "\n";
+}
 
 int main() {
     setlocale(LC_CTYPE, "Russian");
-    double h = 0.5;
-    double e = 1e-1;
-    
-    //methfs(1, 1, 0.5, 3);
-    //methfs(1, 1, 0.25, 3);
-    //for (int i = 0;i < 10;i++){
-      //  methnfs(1, 1, 1, 3, e);
-        //e /= 10;
-    //}
-    e = 1e-9;
-    methnfs(1, 1, 1, 3, e);
+    double h = 0.015625;
+    std::vector<std::pair<double, double>> sol = methnfs(1, 1, h, 3, 1e-8);
+    analyzeSol(sol, 1e-3);
     return 0;
 }
